ee446fb18a93c1507a061790875a0aa63db5a50f
arpit jain <jain.arpit@northeastern.edu>
2024-02-06 14:20:59 -0500

    converion of data types

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,21 @@
+let score = undefined;
+
+let valueInNumber = Number(score);
+
+// console.log(typeof valueInNumber);
+
+// console.log(valueInNumber);
+
+let isLoogedIn = 1;
+
+let booleanisLoggedIn = Boolean(isLoogedIn);
+console.log(booleanisLoggedIn);
+
+//1 => true, 0 => false
+//"" = > false
+// "hitesh" => true
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber)
