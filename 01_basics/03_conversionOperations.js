45311bb9704b28ffb5dab35f786ac08f96bf6838
arpit jain <jain.arpit@northeastern.edu>
2024-02-06 14:46:56 -0500

    data types summary

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -9,7 +9,7 @@ let valueInNumber = Number(score);
 let isLoogedIn = 1;
 
 let booleanisLoggedIn = Boolean(isLoogedIn);
-console.log(booleanisLoggedIn);
+// console.log(booleanisLoggedIn);
 
 //1 => true, 0 => false
 //"" = > false
@@ -17,5 +17,25 @@ console.log(booleanisLoggedIn);
 
 let someNumber = 33;
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+/*
+Why string to number conversion is confusing
+ */
+
+// *********************** Operations ***********************
+
+let value = 3;
+let negValue = -value;
+//console.log(negValue)
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2 + 2); //122
+// console.log(1 + 2 + "2"); //32
+
